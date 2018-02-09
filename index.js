4b9fa6ec71520df4202b94e3b30fe52341b51af8
Willievuong <willievuong@gmail.com>
2018-02-08 18:44:38 -0800

    Able to post

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ console.log('Bot is starting');
 
 var twitter = require('twitter'); 
 
-var client = new twitter({
+var config = require('./config.js');
+console.log(config);
+var client = new twitter(config);
 
+/*
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, 
+	function(error, tweets, response){
+		if(!error){
+			console.log(tweets);
+		}
+
+})
+*/
+
+var params = {screen_name: 'CatxBot'};
+client.post('statuses/update', {status:'Lillian is super cool!!!'},
+ function(error, tweet, response){
+ 	if(!error){
+ 		console.log(tweet);
+ 	}else{
+ 		console.log(error);
+ 	}
 });
