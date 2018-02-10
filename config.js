1e1390cdabcda4c342f0e8575b1812224db28494
Willievuong <willievuong@gmail.com>
2018-02-09 16:17:44 -0800

    Testing gitignore

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
-
 module.exports = {
 		consumer_key: 			'qqdKrs9wVHuHnDceacpKSSFWw'
 	, 	consumer_secret: 		'xJUlfpvbOkwiGLEmSJZMADJNu8HL5MnpltbNGpe4BSscpZte7T'
 	, 	access_token_key: 		'957897068445446145-1bA1bXAhaHovllksPIPJaKxDrZqrJLT'
 	, 	access_token_secret: 	'CT6wsEvXSpwDTGnwonhK8SxIM6wP23Fb2d7cWOpx6Zjtn'
 };
-
-//var consumer_key = 'qqdKrs9wVHuHnDceacpKSSFWw';
-//var consumer_secret =  'xJUlfpvbOkwiGLEmSJZMADJNu8HL5MnpltbNGpe4BSscpZte7T';
-//var access_token_key =  '957897068445446145-1bA1bXAhaHovllksPIPJaKxDrZqrJLT';
-//var access_token_secret =  'CT6wsEvXSpwDTGnwonhK8SxIM6wP23Fb2d7cWOpx6Zjtn';
